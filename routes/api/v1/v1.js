a59894ac4651efa3c70c746165d9c82baec0ae14
JonathanA03 <alemanjl.ix@gmail.com>
2022-02-12 09:16:29 -0600

    Avances

--- routes/api/v1/v1.js
+++ routes/api/v1/v1.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { verifyApiHeaderToken } = require("./headerVerifyMiddleware");
 const pacientesRoutes = require("./pacientes/pacientes");
 const expedientesRoutes = require("./expedientes/expedientes");
 //const expredientesRoutes = require('./expredientes/expredientes');
 //middlewares
-router.use("/pacientes", pacientesRoutes);
+router.use("/pacientes", verifyApiHeaderToken, pacientesRoutes);
 router.use("/expedientes", expedientesRoutes);
 //router.use("/expredientes", expredientesRoutes);
 module.exports = router;
